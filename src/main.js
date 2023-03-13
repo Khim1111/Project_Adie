a89ce71033a47c3f539561ba50ec668fd4671967
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-03-13 11:22:52 +0800

    Created discography& about page

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// $(function() {
+//     $('.play-button').on('click', function() {
+//       var video = $(this).closest('.video-container').find('video')[0];
+//       video.play();
+//     });
+  
+//     $('.stop-button').on('click', function() {
+//       var video = $(this).closest('.video-container').find('video')[0];
+//       video.pause();
+//     });
+//   });
