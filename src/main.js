53b6699006add727543174be7665ed9a985f1628
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-03-19 22:34:02 +0800

    Created Gallery page, added image grid inside a carousel

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-// $(function() {
-//     $('.play-button').on('click', function() {
-//       var video = $(this).closest('.video-container').find('video')[0];
-//       video.play();
-//     });
-  
-//     $('.stop-button').on('click', function() {
-//       var video = $(this).closest('.video-container').find('video')[0];
-//       video.pause();
-//     });
-//   });
+// var myCarousel = document.querySelector('#myCarousel')
+// var carousel = new bootstrap.Carousel(myCarousel, {
+//   interval: 2000, // Set the interval for auto-scrolling
+//   wrap: true, // Allow the carousel to loop through the slides
+// })
 
+// // Add event listener to the "pause" button to pause the carousel when clicked
+// document.querySelector('#pauseButton').addEventListener('click', function() {
+//   carousel.pause()
+// })
 
-  
+// // Add event listener to the "play" button to start the carousel when clicked
+// document.querySelector('#playButton').addEventListener('click', function() {
+//   carousel.cycle()
+// })
+
+var myCarousel = document.querySelector('#myCarousel');
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 3000,
+  wrap: true
+});
