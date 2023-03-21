c22a09f760857fb327afea45d46897111dd5d1b1
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-03-22 00:59:01 +0800

    Created Bookings page and added images and forms

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-// var myCarousel = document.querySelector('#myCarousel')
-// var carousel = new bootstrap.Carousel(myCarousel, {
-//   interval: 2000, // Set the interval for auto-scrolling
-//   wrap: true, // Allow the carousel to loop through the slides
-// })
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000, // Set the interval for auto-scrolling
+  wrap: true, // Allow the carousel to loop through the slides
+})
 
-// // Add event listener to the "pause" button to pause the carousel when clicked
-// document.querySelector('#pauseButton').addEventListener('click', function() {
-//   carousel.pause()
-// })
+// Add event listener to the "pause" button to pause the carousel when clicked
+document.querySelector('#pauseButton').addEventListener('click', function() {
+  carousel.pause()
+})
 
-// // Add event listener to the "play" button to start the carousel when clicked
-// document.querySelector('#playButton').addEventListener('click', function() {
-//   carousel.cycle()
-// })
+// Add event listener to the "play" button to start the carousel when clicked
+document.querySelector('#playButton').addEventListener('click', function() {
+  carousel.cycle()
+})
 
 var myCarousel = document.querySelector('#myCarousel');
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 3000,
   wrap: true
+});
+
+$(function() {
+  $('#datepicker').datepicker();
 });
