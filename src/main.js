e2247a182ece8e6f9ad730d636d84f831e700216
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-03-23 05:57:17 +0800

    Adjust Forms spacing in bookings page

--- src/main.js
+++ src/main.js
@@ -1,25 +1,12 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000, // Set the interval for auto-scrolling
-  wrap: true, // Allow the carousel to loop through the slides
-})
+// Get all the navbar links
+const navLinks = document.querySelectorAll('.nav-link');
 
-// Add event listener to the "pause" button to pause the carousel when clicked
-document.querySelector('#pauseButton').addEventListener('click', function() {
-  carousel.pause()
-})
-
-// Add event listener to the "play" button to start the carousel when clicked
-document.querySelector('#playButton').addEventListener('click', function() {
-  carousel.cycle()
-})
-
-var myCarousel = document.querySelector('#myCarousel');
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 3000,
-  wrap: true
-});
-
-$(function() {
-  $('#datepicker').datepicker();
+// Add a click event listener to each link
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // Remove the "active" class from all links
+    navLinks.forEach(link => link.classList.remove('active'));
+    // Add the "active" class to the clicked link
+    link.classList.add('active');
+  });
 });
